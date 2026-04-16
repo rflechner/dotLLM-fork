17547549a09fedb2c2e360a2fedf6af1fefe92e3
rflechner <romain.flechner@proton.me>
2026-04-16 09:51:48 +0200

    chore: build.sh creates .metallib files

--- native/metal/build.sh
+++ native/metal/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+rm -R bin
 mkdir bin
 
 clang++ \
@@ -13,3 +14,6 @@ clang++ \
   -framework Metal \
   -o bin/libdotllmmetal.dylib
 
+# Dans build.sh
+xcrun -sdk macosx metal -c add.metal -o bin/add.air
+xcrun -sdk macosx metallib bin/add.air -o add.metallib
